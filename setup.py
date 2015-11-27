97ce4749c9a0701712a3cbf7e56b70f8d9ebfdc0
Zhongwei Sun <zhongwei.sun2008@gmail.com>
2015-11-27 15:24:34 +0800

    modify dir stucture

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 setup(
     name='rlog',
     packages=['rlog'],
-    version='0.1',
+    version='0.2',
     description='rlog - log module for humans.',
     author='Zhongwei Sun',
     author_email='zhongwei.sun2008@gmail.com',
-    url='https://github.com/sunzhongwei/logpy',
+    url='https://github.com/sunzhongwei/rlog',
     keywords=['logging', ],
 )
