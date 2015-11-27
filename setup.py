47b647d0e3f650c26096f57294a40925a29ced41
Zhongwei Sun <zhongwei.sun2008@gmail.com>
2015-11-27 17:47:49 +0800

    fix package issue

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='ytlog',
     packages=['ytlog'],
-    version='0.4',
+    version='0.6',
     description='ytlog - log module for humans.',
     author='Zhongwei Sun',
     author_email='zhongwei.sun2008@gmail.com',
---
+++ ytlog/__init__.py
@@ -0,0 +1,3 @@
+from ytlog import get_logger
+
+__all__ = ['get_logger', ]
